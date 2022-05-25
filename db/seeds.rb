0c0d32793b80d734b2015ad62df3740760abcdef
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2022-05-25 19:38:06 +0100

    updated seeds to create plants

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 puts "Cleaning database..."
+Plant.destroy_all if Rails.env.development?
 Garden.destroy_all if Rails.env.development?
 
 puts "Creating gardens..."
@@ -14,3 +15,25 @@ Garden.create!(
 )
 
 puts "Finished! You have #{Garden.count} gardens"
+
+puts "Creating plants..."
+
+Plant.create!(
+  name: 'Monstera',
+  image_url: "https://images.unsplash.com/photo-1585598117791-876ce25c1884?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974",
+  garden: Garden.first
+)
+
+Plant.create!(
+  name: 'Palm Tree',
+  image_url: "https://images.unsplash.com/photo-1574172977553-4968cf5aaead?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870",
+  garden: Garden.first
+)
+
+Plant.create!(
+  name: 'Strangler Fig',
+  image_url: "https://www.science.org/do/10.1126/science.aan6932/full/GettyImages-183868665_16x9.jpg",
+  garden: Garden.first
+)
+
+puts "Finished! You have #{Plant.count} plants"
